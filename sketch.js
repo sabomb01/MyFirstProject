a68b9c0c442c01845832bfda70a72a2f40eb3d05
Sam Bondurant <124181@bwls.net>
2016-10-13 15:13:47 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 function setup() {
   createCanvas( 800 , 800 );
-}
-
-function draw() {
   var c = color(255, 204, 0);  
 fill(c);  
 noStroke();  
-ellipse(25, 25, 80, 80);  
+}
+
+function draw() {
+  ellipse(25, 25, 80, 80);  
 
 }
